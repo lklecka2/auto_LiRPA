28da3d0148ce320142f4a50b485ac71bca7acc3b
Zhouxing Shi <zhouxingshichn@gmail.com>
2025-01-17 00:18:53 -0800

    Jan 2025 prerelease
    Co-authored-by: Huan Zhang <huan@huan-zhang.com>
    Co-authored-by: Christopher Brix <brix@cs.rwth-aachen.de>
    Co-authored-by: JojaCola312 <169118242+JojaCola312@users.noreply.github.com>
    Co-authored-by: xiangruzh <67789819+xiangruzh@users.noreply.github.com>
    Co-authored-by: Clause DZ <duozhou2@illinois.edu>
    Co-authored-by: jorgejc2 <jorgejc2@illinois.edu>
    Co-authored-by: ruizegao <46378029+ruizegao@users.noreply.github.com>
    Co-authored-by: HaoChen <148925050+playboy233@users.noreply.github.com>
    Co-authored-by: Kirkxuhj <68587847+Hongji1001@users.noreply.github.com>
    Co-authored-by: Kaidi Xu <xukd2015@gmail.com>

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 # Check PyTorch version
-pytorch_version_l = '1.11.0'
-pytorch_version_u = '2.3.0' # excluded
+pytorch_version_l = '2.0.0'
+pytorch_version_u = '2.4.0' # excluded
 torchvision_version_l = '0.12.0'
-torchvision_version_u = '0.18.0' # excluded
+torchvision_version_u = '0.19.0' # excluded
 msg_install_pytorch = (f'It is recommended to manually install PyTorch '
                     f'(>={pytorch_version_l},<{pytorch_version_u}) suitable '
                     'for your system ahead: https://pytorch.org/get-started.\n')
@@ -42,15 +42,17 @@ setup(
     install_requires=[
         f'torch>={pytorch_version_l},<{pytorch_version_u}',
         f'torchvision>={torchvision_version_l},<{torchvision_version_u}',
-        'numpy>=1.20',
+        'numpy>=1.20,<2.0',
         'packaging>=20.0',
-        'pytest>=5.0',
+        'pytest==8.1.1',
         'pylint>=2.15',
         'pytest-order>=1.0.0',
+        'pytest-mock>=3.14',
         'appdirs>=1.4',
         'pyyaml>=5.0',
-        'ninja>=1.10',
+        'ninja>=1.10,<1.11.1.2',
         'tqdm>=4.64',
+        'graphviz>=0.20.3'
     ],
     platforms=['any'],
     license='BSD',
